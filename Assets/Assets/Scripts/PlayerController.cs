4e49da538345e6faaba7c495e9bc5be5904972f0
Gabriel Teles Beserra <gabrieltbeserra@gmail.com>
2021-05-29 19:12:04 -0300

    Adicionado mira para se achar no mapa

--- Assets/Assets/Scripts/PlayerController.cs
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -1,47 +1,36 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    
     private Animator _animator;
     private Rigidbody _rigidbody;
-    private NavMeshAgent agent;
-
-    private Camera cam;
-
-    private float camInitX;
-    private float camInitY;
-
+    private NavMeshAgent _agent;
+    private Camera _camera;
 
     private void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        _agent = GetComponent<NavMeshAgent>();
         _animator = GetComponent<Animator>();
         _rigidbody = GetComponent<Rigidbody>();
-        cam = Camera.main;
-        camInitX = cam.transform.position.x;
-        camInitY = cam.transform.position.y;
+        _camera = Camera.main;
     }
-    Vector3 lastPosition;
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            var ray = cam.ScreenPointToRay(Input.mousePosition);
+            var ray = _camera.ScreenPointToRay(Input.mousePosition);
 
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, 1000f)) agent.SetDestination(hit.point);
+            if (Physics.Raycast(ray, out hit, 1000f)) _agent.SetDestination(hit.point);
         }
-
-
-        //cam.transform.position = new Vector3(target.transform.position.x + camInitX, target.transform.position.y + camInitY, 0);
 
 
         //_animator.SetFloat("speed", 1, 0.2f, Time.deltaTime);
         //_animator.SetFloat("turn", 1, 0.2f, Time.deltaTime);
-
     }
 
     private void OnAnimatorMove()
